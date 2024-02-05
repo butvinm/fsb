e8d30332747f34b06223af177079d47ff5c0aa72
Mihail Butvin <butvin.mihail@yandex.ru>
2024-02-05 11:06:43 +0300

    Init

---
+++ README.md
@@ -0,0 +1,39 @@
+# Fancy String Builder
+
+**Not StringBuilder you want, but StringBuilder I deserve**
+
+Opinionated StringBuilder wrapper.
+
+It was designed for brevity, readability and convenience.
+
+Example:
+
+```java
+var fsb = new FancyStringBuilder();
+
+fsb.l("package %s;", targetPackage);
+fsb.n();
+fsb.l("import lombok.Data;");
+fsb.n();
+fsb.l("/**").l(spec.getDescription()).l("*/");
+fsb.l("@Data");
+fsb.l("public class %s ", spec.getName()).a("{").bb();
+for (var field : spec.getFields()) {
+    fsb
+        .l("/**")
+        .l(field.getDescription()).l("*/")
+        .l("private final %s %s;", field.getType(), field.getName())
+        .n();
+}
+fsb.eb().l("}");
+
+return fsb.toString();
+```
+
+For more details refer to source code.
+
+
+## Installation
+
+It's single 100 LOC class - just copy-paste it to your project.
+
---
+++ src/test/java/butvinm/fsb/FancyStringBuilderTest.java
@@ -0,0 +1,104 @@
+package butvinm.fsb;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class FancyStringBuilderTest {
+    private FancyStringBuilder fsb;
+
+    @BeforeEach
+    public void setUp() {
+        fsb = new FancyStringBuilder();
+    }
+
+    @Test
+    public void testAppendObject() {
+        fsb.a(123);
+        assertEquals("123", fsb.toString());
+    }
+
+    @Test
+    public void testAppendStringWithFormatting() {
+        fsb.a("Value: %d", 123);
+        assertEquals("Value: 123", fsb.toString());
+    }
+
+    @Test
+    public void testAppendObjectWithLineBreak() {
+        fsb.l(123);
+        assertEquals("123\n", fsb.toString());
+    }
+
+    @Test
+    public void testAppendFormattedStringWithLineBreak() {
+        fsb.l("Value: %d", 123);
+        assertEquals("Value: 123\n", fsb.toString());
+    }
+
+    @Test
+    public void testAppendSpace() {
+        fsb.s();
+        assertEquals(" ", fsb.toString());
+    }
+
+    @Test
+    public void testAppendLineBreak() {
+        fsb.n();
+        assertEquals("\n", fsb.toString());
+    }
+
+    @Test
+    public void testAppendTabulation() {
+        fsb.t();
+        assertEquals("\t", fsb.toString());
+    }
+
+    @Test
+    public void testIndentation() {
+        fsb
+            .bb()
+                .l("Indented")
+                .a("Indented").s().a("Same line")
+            .eb()
+            .a("Not indented");
+
+        assertEquals(
+            "\tIndented\n\tIndented Same lineNot indented",
+            fsb.toString()
+        );
+    }
+
+    @Test
+    public void testComplexScenario() {
+        var expected = "" +
+            "You can make classes,\n " +
+            "but you can only ever make one instance of them.\n" +
+            "\t\tThis shouldn't affect how most object-oriented programmers work" +
+            ".\n" +
+            ":3\n";
+
+        fsb
+            .l("You can make classes,").s()
+            .a("but %s %s", "you can only", "ever make").s()
+            .a("one instance of them.")
+            .n()
+            .bb()
+            .t()
+            .a("This shouldn't affect how most object-oriented programmers work")
+            .eb()
+            .a(".").n()
+            .l(":3");
+
+        assertEquals(expected, fsb.toString());
+    }
+
+    @Test
+    public void testNegativeIndentationThrowsException() {
+        FancyStringBuilder builder = new FancyStringBuilder();
+        assertThrows(RuntimeException.class, () -> {
+            builder.eb();
+        });
+    }
+}
